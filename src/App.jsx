e49de409c294028f7baf683475cba856f8ac677c
Alfina Balqis Nurzaharani <alfinabalqis28@gmail.com>
2023-07-17 15:40:49 +0700

    add tailwind css and styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import Card from './components/Card';
 
 function App() {
   return (
-    <div>
+    <div class="container m-auto">
       <header>
         <h1>Ninja Merch</h1>
       </header>
 
-      <img src={banner} alt="Site Banner" />
-      <Card/>
-      <Card/>
-      <Card/>
+      <img class="rounded-md" src={banner} alt="Site Banner" />
+
+      <div class="grid grid-cols-4 gap-10 my-4">
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,9 @@
 export default function Card(){
     return(
-        <div>
+        <div class="bg-white p-4 text-center rounded-md shadow-ms">
             <h2>Card Component</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias architecto assumenda provident autem. Quibusdam eum quos aut aspernatur odit libero officiis veritatis quisquam accusantium neque labore laborum, sapiente officia blanditiis.</p>
+            <button class="btn">Click Me</button>
         </div>
     )
 }
