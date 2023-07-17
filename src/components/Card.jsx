b4301762990970d7ca0b2a5b62ad1bbf799cbc6f
Alfina Balqis Nurzaharani <alfinabalqis28@gmail.com>
2023-07-17 15:17:53 +0700

    add component

---
+++ src/components/Card.jsx
@@ -0,0 +1,8 @@
+export default function Card(){
+    return(
+        <div>
+            <h2>Card Component</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias architecto assumenda provident autem. Quibusdam eum quos aut aspernatur odit libero officiis veritatis quisquam accusantium neque labore laborum, sapiente officia blanditiis.</p>
+        </div>
+    )
+}
